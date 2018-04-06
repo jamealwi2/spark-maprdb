0da1cc208ea0dde921d5fbbc5005638ab1654587
Alwin James <38123613+jamealwi2@users.noreply.github.com>
2018-04-06 16:45:28 -0700

    Update FlightAnalysis.scala

--- src/main/scala/flightanalysis/FlightAnalysis.scala
+++ src/main/scala/flightanalysis/FlightAnalysis.scala
@@ -1,3 +1,4 @@
+// Import necessary packages
 import org.apache.spark.sql.Dataset
 import org.apache.spark.rdd.RDD
 import com.mapr.db.MapRDB
@@ -7,9 +8,12 @@ import com.mapr.db.spark.impl.OJAIDocument
 
 object FlightAnalysis {
 
+  // Case Class containing the Year, Month, DayofMonth, Scheduled departure time(CRSDepTime), 
+  // FlightNumber, Origin, Destination, Cancellation status and Cancellation code.
   case class CancelledFlights(Year: String,
                               Month: String,
                               DayofMonth: String,
+                              CRSDepTime: String,
                               FlightNum: String,
                               Origin: String,
                               Dest: String,
@@ -17,15 +21,19 @@ object FlightAnalysis {
                               CancellationCode: String)
       extends Serializable
 
+  // Case Class containing the Year, Month, DayofMonth, Scheduled departure time(CRSDepTime), 
+  // FlightNumber, Origin, Destination and Cancellation reason.
   case class CancelledFlightsWithReason(Year: String,
                                         Month: String,
                                         DayofMonth: String,
+                                        CRSDepTime: String,
                                         FlightNum: String,
                                         Origin: String,
                                         Dest: String,
                                         CancellationReason: String)
       extends Serializable
 
+  // Case class defining the MapRDB-JSON document. 
   case class FlightJsonDocument(_id: String,
                                 FlightNum: String,
                                 Origin: String,
@@ -33,29 +41,31 @@ object FlightAnalysis {
                                 CancellationReason: String)
       extends Serializable
 
+  // Function creating the _id for MapRDB JSON
   def createJsonDocument(r: CancelledFlightsWithReason): FlightJsonDocument = {
-    val _id = r.Year + "_" + r.Month + "_" + r.DayofMonth
+    val _id = r.Year + "_" + r.Month + "_" + r.DayofMonth + "#" + r.FlightNum + "#" + r.CRSDepTime
     FlightJsonDocument(_id, r.FlightNum, r.Origin, r.Dest, r.CancellationReason)
   }
 
   def main(args: Array[String]) {
 
+    // Reading the input data
     val csvfile = "/tmp/flightdata/2008.csv"
     var csvDF = spark.read.option("header", "true").csv(csvfile)
+    
+    // Creating a temporary view to query only necessary fields.
     csvDF.createOrReplaceTempView("flightdata")
+    
+    // Filtering the necessary information.
+    // We need only subset of fields from the available 27 fields.
+    // Also, we need information only for the cancelled flights.
+    // Cancelled flights will have 'Cancelled=1'.
     val cancelledFlightsDS: Dataset[CancelledFlights] = spark
       .sql(
-        "select Year,Month,DayofMonth,FlightNum,Origin,Dest,Cancelled,CancellationCode from flightdata where Cancelled='1'")
+        "select Year,Month,DayofMonth,CRSDepTime,FlightNum,Origin,Dest,Cancelled,CancellationCode from flightdata where Cancelled='1'")
       .as[CancelledFlights]
 
-    val cancelledFlightsWithReasonDS = cancelledFlightsDS.withColumn(
-      "CancellationReason",
-      lookup(cancelledFlightsDS("CancellationCode")))
-    spark
-      .sql(
-        "select Year,Month,DayofMonth,FlightNum,Origin,Dest,Cancelled,CancellationCode from flightdata where Cancelled='1' limit 2")
-      .show
-
+    // Converting the Cancellation code to more meaningful reason.
     val lookup = udf[String, String](cancellationCode =>
       cancellationCode match {
         case "A" => "carrier"
@@ -64,7 +74,8 @@ object FlightAnalysis {
         case "D" => "security"
         case _   => "unknown"
     })
-
+    
+    
     val cancelledFlightsWithReasonDS = cancelledFlightsDS.withColumn(
       "CancellationReason",
       lookup(cancelledFlightsDS("CancellationCode")))
@@ -73,15 +84,17 @@ object FlightAnalysis {
     val cancelledFlightsWithReasonDS2: Dataset[CancelledFlightsWithReason] =
       spark
         .sql(
-          "select Year,Month,DayofMonth,FlightNum,Origin,Dest,CancellationReason from cancelledflightdata")
+          "select Year,Month,DayofMonth,CRSDepTime,FlightNum,Origin,Dest,CancellationReason from cancelledflightdata")
         .as[CancelledFlightsWithReason]
 
+    // Creating OJAI MapRDB-JSON RDD
     val ojaiRDD = cancelledFlightsWithReasonDS2
       .map(createJsonDocument)
       .toJSON
       .rdd
       .map(MapRDBSpark.newDocument)
 
+    // Storing the data to MapRDB-JSON table.
     ojaiRDD.saveToMapRDB("/tmp/flighttable",
                          createTable = true,
                          idFieldPath = "_id")
